517981bc8c93d81ce968e64ac7f73470bf0b79ce
Héctor Ruiz <ruac.hector@gmail.com>
2022-11-17 12:21:00 +0100

    Testing new output

--- vars/lambdaLayersCleaner.groovy
+++ vars/lambdaLayersCleaner.groovy
@@ -10,40 +10,47 @@ def call(ENVIRONMENT, MAXVERSIONS) {
   env.MAXVERSIONS = MAXVERSIONS
 
   sh '''#!/bin/bash
-    set +x
-    if [ ${ENVIRONMENT} != 'pro' ] && [ ${ENVIRONMENT} != 'stg' ] && [ ${ENVIRONMENT} != 'dev' ];
+  set +x
+  layersName=( $(aws lambda list-layers | jq -r ".Layers[].LayerName") )
+  for layer in ${layersName[@]}
+  do
+    lambdaLayerVersions=( $(aws lambda list-layer-versions --layer-name $layer | jq -r ".LayerVersions[].LayerVersionArn") )
+
+    if [[ ${#lambdaLayerVersions[@]} -le ${MAXVERSIONS} ]]
     then
-      exit 1
+      echo "Layer '${layer}' doesn't surpass the version limit"
+      continue
     fi
-    layersName=( $(aws lambda list-layers | jq -r ".Layers[].LayerName") )
-    for layer in ${layersName[@]}
+
+    printf "\n-----------------------------------------------------\n\n"
+    echo "List of all layers versions of: ${layer}"
+    for i in ${lambdaLayerVersions[@]}
     do
-      echo "List of all layers versions of: ${layer}"
-      lambdaLayerVersions=( $(aws lambda list-layer-versions --layer-name $layer | jq -r ".LayerVersions[].LayerVersionArn") )
-      for i in ${lambdaLayerVersions[@]}
-      do
-        echo $i
-      done
-
-      layersToDelete=()
-
-      while [ ${#lambdaLayerVersions[@]} -gt ${MAXVERSIONS} ]
-      do
-        layerNameVersion=$(echo ${lambdaLayerVersions[${#lambdaLayerVersions[@]}-1]} | cut -d: -f7-8)
-        layersToDelete+=(${layerNameVersion})
-
-        unset "lambdaLayerVersions[${#lambdaLayerVersions[@]}-1]"
-      done
-
-      echo "The following layers will be deleted: ${layersToDelete[*]}"
-      for layer in ${layersToDelete[@]} {
-        echo $layer
-        #layer_name=$(echo ${layer} | cut -d: -f1)
-        #version=$(echo ${layer}| cut -d: -f2)
-        #aws lambda delete-layer-version --layer-name ${layer_name} --version-number ${version} 
-        #echo "The following lamda layer version has been deleted: ${layer_name}:${version}" 
-      }
+      echo $i
     done
+
+    layersToDelete=()
+
+    while [ ${#lambdaLayerVersions[@]} -gt ${MAXVERSIONS} ]
+    do
+      layerNameVersion=$(echo ${lambdaLayerVersions[${#lambdaLayerVersions[@]}-1]} | cut -d: -f7-8)
+      layersToDelete+=(${layerNameVersion})
+
+      unset "lambdaLayerVersions[${#lambdaLayerVersions[@]}-1]"
+    done
+
+    echo ""
+    echo "The following layers will be deleted: ${layersToDelete[*]}"
+
+    for layer in ${layersToDelete[@]}
+    do
+      layer_name=$(echo ${layer} | cut -d: -f1)
+      version=$(echo ${layer}| cut -d: -f2)
+      #aws lambda delete-layer-version --layer-name ${layer_name} --version-number ${version}
+      echo "The following lambda layer version has been deleted: ${layer_name}:${version}"
+    done
+    printf "\n-----------------------------------------------------\n\n"
+  done
   '''
 }
 
